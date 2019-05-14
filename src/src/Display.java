5d4e2d282e6fa54b8d3eba0bbca0acb57fddcce1
Chami L <chami.lamelas@gmail.com>
2019-05-14 15:43:36 +0100

    fixed csv path-writing bug, tweaked Display

--- src/src/Display.java
+++ src/src/Display.java
@@ -3,13 +3,21 @@ package src;
 import java.io.File;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class Display {
-
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e) {
+			JOptionPane.showMessageDialog(null, "There was an error loading the system's look and feel.", "Error!", JOptionPane.ERROR_MESSAGE);
+		}
 		JFileChooser chooser = new JFileChooser();
 		chooser.setMultiSelectionEnabled(true);
+		chooser.setCurrentDirectory(new File(System.getProperty("user.home"))); // for windows this is C:\Users\<System.getProperty("user.name")>
 		FileNameExtensionFilter filter = new FileNameExtensionFilter("JPEG Images", "jpg", "jpeg");
 		chooser.addChoosableFileFilter(filter);
 		int choice = chooser.showOpenDialog(null);
@@ -19,9 +27,9 @@ public class Display {
 			for (File f : files) {
 				rdr.read(f);
 				rdr.calculateRegionOfInterest();
-				int split = f.getName().indexOf(".jpg");
-				if (split == -1) {
-					split = f.getName().indexOf(".jpeg");
+				int split = f.getAbsolutePath().indexOf(".jpg");
+				if (split < 0) {
+					split = f.getAbsolutePath().indexOf(".jpeg");
 				}
 				rdr.writeRegionOfInterestToCSV(f.getAbsolutePath().substring(0, split) + ".csv");
 			}
